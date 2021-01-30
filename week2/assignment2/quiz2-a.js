43da2b718bcdc915d247d768c9b1205aabdadf7a
karimariewhite <49420748+karimariewhite@users.noreply.github.com>
2021-01-29 23:35:17 -0600

    Assignment 2 Second Commit
    Rephrasing the questions to add the context for the TV show FRIENDS.

--- week2/assignment2/quiz2-a.js
+++ week2/assignment2/quiz2-a.js
@@ -1,10 +1,10 @@
 const quiz = [
-    { question: 'Which Friends character has a twin?', answer: 'Phoebe' },
-    { question: 'Who was Monica\'s first kiss?', answer: 'Ross. She called him her Midnight Mystery Kisser' },
-    { question: 'How many times was Ross married?', answer: 'Three (Carol, Emily, & Rachel' },
-    { question: 'Who does Gunther have a crush on?', answer: 'Rachel' },
-    { question: 'Who hates Thanksgiving?', answer: 'Chandler' },
-    { question: 'What book does Joey keep in the Freezer?', answer: 'THE SHINING. Bonus points if you included LITTLE WOMEN' }
+    { question: 'Which FRIENDS character has a twin?', answer: 'Phoebe' },
+    { question: 'On the TV show FRIENDS, who was Monica\'s first kiss?', answer: 'Ross. She called him her Midnight Mystery Kisser' },
+    { question: 'On the TV show FRIENDS, how many times was Ross married?', answer: 'Three (Carol, Emily, & Rachel' },
+    { question: 'On the TV show FRIENDS, who does Gunther have a crush on?', answer: 'Rachel' },
+    { question: 'Which FRIENDS character hates Thanksgiving?', answer: 'Chandler' },
+    { question: 'On the TV show FRIENDS, what book does Joey keep in the Freezer?', answer: 'THE SHINING. Bonus points if you included LITTLE WOMEN' }
 ];
 
 const randomIdx = Math.round(Math.random() * quiz.length - 1);
