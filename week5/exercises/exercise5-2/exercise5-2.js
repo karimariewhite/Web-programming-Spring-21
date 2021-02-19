77f49691ccc8999c3845f54bdb730283502c48f0
karimariewhite <49420748+karimariewhite@users.noreply.github.com>
2021-02-18 23:58:38 -0600

    Complete Ex 2
    Adding for loop and array

--- week5/exercises/exercise5-2/exercise5-2.js
+++ week5/exercises/exercise5-2/exercise5-2.js
@@ -1,33 +1,44 @@
 /* eslint-disable indent */
-let square;
+const mySquares = [];
+const startX = 250;
+const startY = 250;
+const squareW = 50;
+const squareH = 50;
 
 function setup () {
     createCanvas(500, 500);
-    square = new Square(); // defining variable as the "template"
+    for (let i = 0; i < 7; i++) {
+        const square = new Square(startX, startY, squareW, squareH); 
+        mySquares.push(square);
+    }
 }
 
 function draw () {
-    background(0); // put background here so it draws over the previous shape.
-    square.move();
-    square.show();
+    background(0); 
+    for (let j = 0; j < 7; j++) {
+        mySquares[j].move();
+        mySquares[j].show();
     }
+ }
 
 class Square {
-    constructor () {
-        this.x = 250;
-        this.y = 250;
-        this.width = 50;
-        this.height = 50;
+    constructor (startX, startY, squareW, squareH) {
+        this.x = startX;
+        this.y = startY;
+        this.width = squareW;
+        this.height = squareH;
+        this.show();
+        this.move();
     }
 
     // defines how I want the square to move on the canvas every time it redraws.
     move () {
-        this.x = this.x + random (-5, 5);
+        this.x = this.x + random(-5, 5);
         this.y = this.y + random(-5, 5);
     }
 
     // Defines how I want the square to look. At this point I've only defined its location and dimensions.
-    show() {
+    show () {
         noFill();
         stroke(255);
         strokeWeight(2);
