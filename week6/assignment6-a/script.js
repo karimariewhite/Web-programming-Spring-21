73c500969f85140741d86e461eb32dd86a476eca
karimariewhite <49420748+karimariewhite@users.noreply.github.com>
2021-02-28 16:59:59 -0600

    commit for week 6 assignment

---
+++ week6/assignment6-a/script.js
@@ -0,0 +1,17 @@
+new Vue({
+  el: '#llamafacts',
+  data: {
+    family: 'Llamas are members of the camelid family meaning they\'re pretty closely related to vicuñas and camels.',
+    grow: 'Llamas can grow as much as 6 feet tall though the average llama between 5 feet 6 inches and 5 feet 9 inches tall.',
+    weight: 'Llamas weigh between 280 and 450 pounds and can carry 25 to 30 percent of their body weight, so a 400-pound male llama can carry about 100 to 120 pounds on a trek of 10 to 12 miles with no problem.',
+    environment: 'Llamas are hardy and well suited to harsh environments. They are quite sure-footed, easily navigating rocky terrain at high altitudes.',
+    intelligent: 'Llamas are smart and easy to train.',
+    guard: 'Llamas have been used as guard animals for livestock like sheep or even alpacas in North America since the \'80s. They require almost no training to be an effective guard.',
+    spit: 'Llamas don\'t bite. They spit when they\'re agitated, but that\'s mostly at each other. Llamas also kick and neck wrestle each other when agitated.',
+    social: 'Llamas are social animals and prefer to live with other llamas or herd animals. The social structure of llamas changes frequently and a male llama can move up the social ladder by picking, and winning, small fights with the leader of the group.',
+    group: 'A group of llamas is called a herd.',
+    yarn: 'Yarn made from llama fiber is soft and lightweight, yet remarkably warm. The soft, undercoat is used for garments and handicrafts while the coarse, outer coat is frequently used for rugs and ropes.',
+    difference: 'Trying to tell the difference between a llama and an alpaca? Two obvious things to look for: Llamas are generally about twice the size of alpacas, and alpacas have short, pointy ears, whereas llamas have much longer ears that stand straight up and give them an alert look',
+    alpaca: false
+  }
+});
