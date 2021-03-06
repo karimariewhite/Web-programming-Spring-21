a3468d6802c5cb63f42ef8f8e42fe11e303a5bbf
karimariewhite <49420748+karimariewhite@users.noreply.github.com>
2021-03-05 23:14:14 -0600

    Exercise7-1 first commit

---
+++ week7/exercise7-1/script.js
@@ -0,0 +1,16 @@
+// new instance of the Vue class
+new Vue({
+    el: "#llamasvalpacas",
+    data: {
+      textStyle: {
+        padding: "0px 15px"
+      },
+      currentPage: "comparison",
+      llamaWiki: "https://en.wikipedia.org/wiki/Llama",
+      alpacaWiki: "https://en.wikipedia.org/wiki/Alpaca",
+      differences1: "Alpacas and llamas are two of the four lamoid species—the other two species, vicuña and guanaco, are their wild cousins. Interestingly, all four species are able to interbreed and create fertile offspring. While often conflated, alpacas and llamas differ in key ways. The most-distinguishing physical differences between alpacas and llamas are their size, their hair, and their face shapes. Also, they differ in disposition, which affects the way humans have used them over the years.",
+      differences2: "The most-noticeable difference between the two animals is their sizes. Alpacas are smaller, around 90 cm (35 inches) high at the shoulder and between 55 and 65 kg (121 to 143 pounds). Llamas are the biggest lamoid at about 120 cm (47 inches) at the shoulder and about 113 kg (250 pounds). So llamas are going to be a lot bigger than their cousins. Their faces are also dissimilar: alpacas have small, blunt faces with short ears, while llamas have more-elongated faces with banana-sized ears.",
+      differences3: "Another key difference is their hair. Alpacas have shaggy hair that is used for fleece production. Their hair color can vary greatly, from whites and light yellows to browns and blacks. Llamas’ hair is coarser, and their wool is considered inferior, but llama breeders are working to create a llama breed with finer, softer hair."
+    }
+  });
+  
