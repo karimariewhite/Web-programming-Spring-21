85808b442520ff96558b409660ec809b1a0e9dec
karimariewhite <49420748+karimariewhite@users.noreply.github.com>
2021-03-15 00:02:57 -0500

    Assignment 8 forms and styling complete

--- week8/assignment8/collection.js
+++ week8/assignment8/collection.js
@@ -2,42 +2,79 @@ var myVue = new Vue({
   el: '#collection',
   // created an array of objects
   data: {
+    titleInput: '',
+    authorInput: '',
+    genreInput: '',
+    yearInput: '',
+    fictionInput: '',
+    idInput: '',
     library: [
       {
         title: 'Storm Front',
         author: 'Jim Butcher',
         genre: 'Urban Fantasy',
         yearPublished: '2002',
-        fiction: true
+        fiction: true,
+        id: '57'
       },
       {
         title: 'Bone Gap',
         author: 'Laura Ruby',
         genre: 'Magical Realism',
         yearPublished: '2015',
-        fiction: true
+        fiction: true,
+        id: '254'
       },
       {
         title: 'Bird by Bird: Some Instructions on Writing and Life',
         author: 'Anne Lamott',
         genre: 'Writing Reference',
         yearPublished: '1995',
-        fiction: false
+        fiction: false,
+        id: '127'
       },
       {
         title: 'A Cold Day In The Sun',
         author: 'Sara Biren',
         genre: 'Contemporary Romance',
         yearPublished: '2019',
-        fiction: true
+        fiction: true,
+        id: '324'
       },
       {
         title: 'Leaders Eat Last: Why Some Teams Pull Together and Others Don\'t',
         author: 'Simon Sinek',
         genre: 'Work Culture',
         yearPublished: '2014',
-        fiction: false
+        fiction: false,
+        id: '125'
       }
     ]
+  },
+  // method to delete items from list -- bookObject is newly assigned
+  // this filters makes a new array and compares the ID in the array to the ID of the book Object defined above
+  // how could I assign id to book object randomly in future?
+  methods: {
+    deleteItem: function (bookObject) {
+      this.library = this.library.filter(function (books) {
+        if (books.id !== bookObject.id) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+    },
+    addItem: function () {
+      let newItem = {
+        title: this.titleInput,
+        author: this.authorInput,
+        genre: this.genreInput,
+        yearPublished: this.yearInput,
+        fiction: this.fictionInput,
+        id: this.idInput
+      };
+      this.library.push(newItem);
+      this.titleInput = this.authorInput = this.genreInput = this.yearInput = this.fictionInput = this.idInput = "";
+    }
   }
 });
