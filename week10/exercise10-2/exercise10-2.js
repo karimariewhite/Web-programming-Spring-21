a2541c4a3c865252cd352d6338f7cdf37c1ae752
karimariewhite <49420748+karimariewhite@users.noreply.github.com>
2021-04-11 03:17:40 -0500

    exercise 10-2 commit

---
+++ week10/exercise10-2/exercise10-2.js
@@ -0,0 +1,38 @@
+/* eslint-disable indent */
+Vue.component('streaming-track', {
+    template: `<div v-bind:class="['track', trending]">
+        <h3>{{track.title}}</h3>
+        <small> Weeks on Chart: {{track.position.weeksOnChart}} </small>
+        <div><img v-bind:src="track.cover" alt="#"></div>
+        <small><em>{{track.artist}}</em></small>
+    </div>`,
+    props: ['track'],
+    computed: {
+        trending: function () {
+            const delta = this.track.rank - this.track.position.positionLastWeek
+            if (delta > 0) {
+                return 'up'
+            } else if (delta < 0) {
+                return 'down'
+            } else {
+                return 'no-change'
+            }
+        }
+    }
+});
+
+const vm = new Vue({
+    el: '#musicApp',
+    data: {
+        tracks: []
+    },
+    mounted () {
+        axios
+            .get('./data/music-list.json')
+            .then(response => {
+                console.log('response', response);
+                vm.tracks = response.data;
+                console.log(vm.tracks);
+            });
+    }
+})
