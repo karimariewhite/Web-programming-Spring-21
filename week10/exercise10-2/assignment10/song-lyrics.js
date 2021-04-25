1ef38302b1122ac5ecaf4ef9c05b3bf777e6413b
karimariewhite <49420748+karimariewhite@users.noreply.github.com>
2021-04-25 12:08:01 -0500

    First commit

---
+++ week10/exercise10-2/assignment10/song-lyrics.js
@@ -0,0 +1,24 @@
+/* eslint-disable indent */
+var baseURL = 'https://api.lyrics.ovh/v1/';
+const vm = new Vue({
+    el: '#songApp',
+    data: {
+        artist: '',
+        title: '',
+        lyrics: ''
+    },
+    computed: {
+        lyricQuery: function () {
+            return baseURL + this.artist + '/' + this.title
+        }
+    },
+    methods: {
+        getLyrics: function () {
+            axios.get(vm.lyricQuery)
+            .then(response => {
+                console.log('response', response);
+                vm.lyrics = response.data.lyrics;
+        });
+      }
+    }
+})
